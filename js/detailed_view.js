08e83aeedc60b9249e7bce28d1a1b9fb990cd181
Eva <jenny.rudream@gmail.com>
2022-01-21 22:54:14 +0300

    id in url inserted

--- js/detailed_view.js
+++ js/detailed_view.js
@@ -5,7 +5,8 @@ for (let posterImage of posterImages) {
     posterImage.addEventListener('click', openDetailedView) ;
   }
 function openDetailedView(event) {
-    console.log(event.target);
-    document.location.href = "filmography_single.php";
-}
+    console.log(event.target.id);
+   //здесь получить id эвента и добавить его в адресную строку location
+    document.location.href = `filmography_single.php?id=${event.target.id}`;
   
+}
