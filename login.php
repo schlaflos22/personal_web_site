332fdc586922c2db87d65eda27ee2f320255208d
Eva <jenny.rudream@gmail.com>
2022-01-25 17:14:16 +0300

    calling the get_users_data() function, extracting data from the users table, comparing the entered data and the data of registered users with administrator rights

--- login.php
+++ login.php
@@ -1,12 +1,13 @@
 <?php
-$login = "Eva";
-$password = "123";
-    if($_POST['login']=== $login && $_POST['password'] === $password) {
-        
-        session_start();
-        $_SESSION['login'] = $_POST['login'];
-        $_SESSION['password'] = $_POST['password'];
-        header('Location: admin_page.php');
+require('db.php');
+$user =  get_users_data();
 
-    }
+if($_POST['login']=== $user['user_name'] && $_POST['password'] === $user['user_password']) {
+        
+    session_start();
+    $_SESSION['login'] = $_POST['login'];
+    $_SESSION['password'] = $_POST['password'];
+    header('Location: admin_page.php');
+
+}
 ?>
