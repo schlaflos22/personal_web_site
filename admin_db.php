605263d5032aedc43f810b0a3df9686c590472a2
Eva <jenny.rudream@gmail.com>
2022-01-26 18:20:01 +0300

    a file has been created, the add_movie_data_in_dataBase function has been added

---
+++ admin_db.php
@@ -0,0 +1,25 @@
+<?php 
+
+function add_movie_data_in_dataBase($array_) {
+
+    
+$conn = mysqli_connect("localhost", "root", "122", "movie_db");
+if (!$conn) {
+  die("Ошибка: " . mysqli_connect_error());
+}
+
+
+$sql = "INSERT INTO movies VALUES ('$array_[0]', '$array_[1]', '$array_[2]', '$array_[3]', '$array_[4]', '$array_[5]','$array_[6]')";
+if(mysqli_query($conn, $sql)){
+    echo "Данные успешно добавлены";
+    
+} else{
+    echo "Ошибка: " . mysqli_error($conn);
+}
+mysqli_close($conn); 
+}
+?>
+
+
+
+
