053691c9b58d478e5be940f3c8d323633ed3b5dd
Eva <jenny.rudream@gmail.com>
2022-02-10 17:58:18 +0300

    fixing bugs when writing files to the database

--- admin_db.php
+++ admin_db.php
@@ -1,7 +1,7 @@
 <?php 
 
 function add_movie_data_in_dataBase($array_) {
-$conn = mysqli_connect("localhost", "root", "root", "movie_db");
+$conn = mysqli_connect("localhost", "root", "122", "movie_db");
 if (!$conn) {
   die("Ошибка: " . mysqli_connect_error());
 }
@@ -16,11 +16,12 @@ mysqli_close($conn);
 }
  
 function add_project_data_in_dataBase($array_) {
-$conn = mysqli_connect("localhost", "root", "root", "movie_db");
+$conn = mysqli_connect("localhost", "root", "122", "movie_db");
 if (!$conn) {
   die("Ошибка: " . mysqli_connect_error());
 }
-$sql = "INSERT INTO projects VALUES ('$array_[0]', '$array_[1]', '$array_[2]', '$array_[3]', '$array_[4]', '$array_[5]','$array_[6]','$array_[7]','$array_[8]','$array_[9]','$array_[10]','$array_[11]','$array_[12]', '$array_[13]','$array_[14]','$array_[15]')";
+$sql = "INSERT INTO projects (`project_image`,`project_logline_h4`,`project_logline`,`project_conflict_h4`,`project_conflict`,`project_idea_h4`,`project_idea`,`project_relevance_h4`,`project_relevance`,`project_reason_h4`,`project_reason`,`project_purpose_h4`, `project_purpose`,`project_uniqueness_h4`, `project_uniqueness`) 
+VALUES ('$array_[0]', '$array_[1]', '$array_[2]', '$array_[3]', '$array_[4]', '$array_[5]','$array_[6]','$array_[7]','$array_[8]','$array_[9]','$array_[10]','$array_[11]','$array_[12]', '$array_[13]','$array_[14]')";
   if(mysqli_query($conn, $sql)){
       echo "Данные успешно добавлены";
       
@@ -29,12 +30,12 @@ $sql = "INSERT INTO projects VALUES ('$array_[0]', '$array_[1]', '$array_[2]', '
   }
   mysqli_close($conn); 
 }
-function add_photos_data_in_dataBase($arr_photos) {
-  $conn = mysqli_connect("localhost", "root", "root", "movie_db");
+function add_photos_data_in_dataBase($file_name,$new_id) {
+  $conn = mysqli_connect("localhost", "root", "122", "movie_db");
   if (!$conn) {
     die("Ошибка: " . mysqli_connect_error());
   }
-  $sql = "INSERT INTO backstage_images VALUES ('$arr_photos[0]', '$arr_photos[1]', '$arr_photos[2]')";
+  $sql = "INSERT INTO backstage_images (`image`,`movie_id`) VALUES ('$file_name', '$new_id')";
   if(mysqli_query($conn, $sql)){
       echo "Данные успешно добавлены";
       
