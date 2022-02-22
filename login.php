521ecfe11f6a805208390fe90b52af8a7d5275cf
Eva <jenny.rudream@gmail.com>
2022-02-22 23:54:09 +0300

    fix bag

--- login.php
+++ login.php
@@ -1,13 +1,20 @@
 <?php
 require('db.php');
-$user =  get_users_data();
 
-if($_POST['login']=== $user['user_name'] && $_POST['password'] === $user['user_password']) {
+$user =  get_users_data($user);
+//var_dump($user['user_name']);
+var_dump($_POST["login"]);
+if($_POST) {
+    if( $user['user_name'] === $_POST['login'] && $user['user_password'] === $_POST['password']) {
         
-    session_start();
-    $_SESSION['login'] = $_POST['login'];
-    $_SESSION['password'] = $_POST['password'];
-    header('Location: admin_page.php');
-
+        session_start();
+        $_SESSION['login'] = $_POST['login'];
+        $_SESSION['password'] = $_POST['password'];
+         header('Location: admin_page.php');
+       
+    } else {
+        echo('NO');
+    }
 }
+
 ?>
