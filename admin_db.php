90dfdf9dbf7a23ea2a04b5474443bf0f0cd93531
Eva <jenny.rudream@gmail.com>
2022-02-22 23:18:29 +0300

    functions have been created to change data in the movies table and the projects table

--- admin_db.php
+++ admin_db.php
@@ -35,7 +35,7 @@ function add_photos_data_in_dataBase($file_name,$new_id) {
   if (!$conn) {
     die("Ошибка: " . mysqli_connect_error());
   }
-  $sql = "INSERT INTO backstage_images (`image`,`movie_id`) VALUES ('$file_name', '$new_id')";
+  $sql = "INSERT INTO backstage_images (`image_`,`movie_id`) VALUES ('$file_name', '$new_id')";
   if(mysqli_query($conn, $sql)){
       echo "Данные успешно добавлены";
       
@@ -43,6 +43,84 @@ function add_photos_data_in_dataBase($file_name,$new_id) {
       echo "Ошибка: " . mysqli_error($conn);
   }
   mysqli_close($conn); 
+  }
+  function change_movie_data_in_dataBase($new_array_,$id) {
+    
+    $conn = mysqli_connect("localhost", "root", "122", "movie_db");
+    if (!$conn) {
+      die("Ошибка: " . mysqli_connect_error());
+    }
+    $sql = "UPDATE movies SET movie_id= '$new_array_[0]',
+    movie_name ='$new_array_[1]',
+    movie_poster= '$new_array_[2]', 
+    movie_description='$new_array_[3]',
+    movie_video ='$new_array_[4]',
+    movie_awards= '$new_array_[5]',
+    movie_festivals= '$new_array_[6]'
+     WHERE `movie_id`= $id;";
+    if(mysqli_query($conn, $sql)){
+        echo "Данные успешно добавлены";
+        
+    } else{
+        echo "Ошибка: " . mysqli_error($conn);
+    }
+    mysqli_close($conn);
+  }
+  function change_photos_data_in_dataBase($file_name,$id,$img_id) {
+    
+    $conn = mysqli_connect("localhost", "root", "122", "movie_db");
+    if (!$conn) {
+      die("Ошибка: " . mysqli_connect_error());
+    }
+   
+//var_dump($file_name);
+//var_dump($img_id);
+
+
+//var_dump($file_name);
+
+  $sql = "UPDATE backstage_images SET image_='$file_name',movie_id = '$id' WHERE movie_id = '$id' AND image_id = '$img_id';" ;
+
+   
+ 
+    if(mysqli_query($conn, $sql)){
+    //  echo "Данные успешно добавлены";
+        
+    } else{
+        echo "Ошибка: " . mysqli_error($conn);
+    }
+    mysqli_close($conn); 
+  }
+  function change_project_data_in_dataBase($array_,$id) {
+    
+    $conn = mysqli_connect("localhost", "root", "122", "movie_db");
+    if (!$conn) {
+      die("Ошибка: " . mysqli_connect_error());
+    }
+    $sql = "UPDATE projects SET project_id= $array_[0],
+    project_image ='$array_[1]',
+    project_logline_h4='$array_[2]',
+    project_logline= '$array_[3]',
+    project_conflict_h4= '$array_[4]',
+    project_conflict= '$array_[5]',
+    project_idea_h4 = '$array_[6]',
+    project_idea = '$array_[7]',
+    project_relevance_h4='$array_[8]',
+    project_relevance ='$array_[9]',
+    project_reason_h4 = '$array_[10]',
+    project_reason= '$array_[11]',
+    project_purpose_h4 = '$array_[12]', 
+    project_purpose = '$array_[13]',
+    project_uniqueness_h4 = '$array_[14]', 
+    project_uniqueness = '$array_[15]' 
+    WHERE project_id = '$id'";
+      if(mysqli_query($conn, $sql)){
+          echo "Данные успешно добавлены";
+          
+      } else{
+          echo "Ошибка: " . mysqli_error($conn);
+      }
+      mysqli_close($conn); 
   }
 ?>
 
