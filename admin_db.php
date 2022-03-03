1ee138de26cc7e1e4d72b8d8f133148f84909e4d
Eva <jenny.rudream@gmail.com>
2022-03-03 22:39:45 +0300

    in the change_project_data_in_dataBase and add_project_data_in_dataBase functions, an element of the project_name array has been added, functions for deleting data from the database have been added

--- admin_db.php
+++ admin_db.php
@@ -20,8 +20,8 @@ $conn = mysqli_connect("localhost", "root", "122", "movie_db");
 if (!$conn) {
   die("Ошибка: " . mysqli_connect_error());
 }
-$sql = "INSERT INTO projects (`project_image`,`project_logline_h4`,`project_logline`,`project_conflict_h4`,`project_conflict`,`project_idea_h4`,`project_idea`,`project_relevance_h4`,`project_relevance`,`project_reason_h4`,`project_reason`,`project_purpose_h4`, `project_purpose`,`project_uniqueness_h4`, `project_uniqueness`) 
-VALUES ('$array_[0]', '$array_[1]', '$array_[2]', '$array_[3]', '$array_[4]', '$array_[5]','$array_[6]','$array_[7]','$array_[8]','$array_[9]','$array_[10]','$array_[11]','$array_[12]', '$array_[13]','$array_[14]')";
+$sql = "INSERT INTO projects (`project_name`,`project_image`,`project_logline_h4`,`project_logline`,`project_conflict_h4`,`project_conflict`,`project_idea_h4`,`project_idea`,`project_relevance_h4`,`project_relevance`,`project_reason_h4`,`project_reason`,`project_purpose_h4`, `project_purpose`,`project_uniqueness_h4`, `project_uniqueness`) 
+VALUES ('$array_[0]', '$array_[1]', '$array_[2]', '$array_[3]', '$array_[4]', '$array_[5]','$array_[6]','$array_[7]','$array_[8]','$array_[9]','$array_[10]','$array_[11]','$array_[12]', '$array_[13]','$array_[14]','$array_[15]')";
   if(mysqli_query($conn, $sql)){
       echo "Данные успешно добавлены";
       
@@ -66,7 +66,37 @@ function add_photos_data_in_dataBase($file_name,$new_id) {
     }
     mysqli_close($conn);
   }
-  function change_photos_data_in_dataBase($file_name,$id,$img_id) {
+
+  function delete_movie($id) {
+    $conn = mysqli_connect("localhost", "root", "122", "movie_db");
+    if (!$conn) {
+      die("Ошибка: " . mysqli_connect_error());
+    }
+    $sql = "DELETE FROM movies WHERE `movie_id` = '$id'";
+    if(mysqli_query($conn, $sql)){
+        echo "Данные успешно удалены";
+        
+    } else{
+        echo "Ошибка: " . mysqli_error($conn);
+    }
+    mysqli_close($conn);
+  }
+  function delete_photo($id) {
+    $conn = mysqli_connect("localhost", "root", "122", "movie_db");
+    if (!$conn) {
+      die("Ошибка: " . mysqli_connect_error());
+    }
+    $sql = "DELETE FROM backstage_images WHERE `movie_id` = '$id'";
+    if(mysqli_query($conn, $sql)){
+        echo "Данные успешно удалены";
+        
+    } else{
+        echo "Ошибка: " . mysqli_error($conn);
+    }
+    mysqli_close($conn);
+  }
+
+  function change_photos_data_in_dataBase($file_name,$id) {
     
     $conn = mysqli_connect("localhost", "root", "122", "movie_db");
     if (!$conn) {
@@ -74,23 +104,16 @@ function add_photos_data_in_dataBase($file_name,$new_id) {
     }
    
 //var_dump($file_name);
-//var_dump($img_id);
 
-
-//var_dump($file_name);
-
-  $sql = "UPDATE backstage_images SET image_='$file_name',movie_id = '$id' WHERE movie_id = '$id' AND image_id = '$img_id';" ;
-
-   
- 
-    if(mysqli_query($conn, $sql)){
-    //  echo "Данные успешно добавлены";
-        
+  $sql = "UPDATE `backstage_images` SET `image_`='$file_name',`movie_id` = '$id' WHERE `movie_id` = '$id' " ;
+  if(mysqli_query($conn, $sql)){
+     echo "Данные успешно добавлены";
     } else{
         echo "Ошибка: " . mysqli_error($conn);
     }
     mysqli_close($conn); 
   }
+  
   function change_project_data_in_dataBase($array_,$id) {
     
     $conn = mysqli_connect("localhost", "root", "122", "movie_db");
@@ -98,21 +121,22 @@ function add_photos_data_in_dataBase($file_name,$new_id) {
       die("Ошибка: " . mysqli_connect_error());
     }
     $sql = "UPDATE projects SET project_id= $array_[0],
-    project_image ='$array_[1]',
-    project_logline_h4='$array_[2]',
-    project_logline= '$array_[3]',
-    project_conflict_h4= '$array_[4]',
-    project_conflict= '$array_[5]',
-    project_idea_h4 = '$array_[6]',
-    project_idea = '$array_[7]',
-    project_relevance_h4='$array_[8]',
-    project_relevance ='$array_[9]',
-    project_reason_h4 = '$array_[10]',
-    project_reason= '$array_[11]',
-    project_purpose_h4 = '$array_[12]', 
-    project_purpose = '$array_[13]',
-    project_uniqueness_h4 = '$array_[14]', 
-    project_uniqueness = '$array_[15]' 
+    project_name = '$array_[1]',
+    project_image ='$array_[2]',
+    project_logline_h4='$array_[3]',
+    project_logline= '$array_[4]',
+    project_conflict_h4= '$array_[5]',
+    project_conflict= '$array_[6]',
+    project_idea_h4 = '$array_[7]',
+    project_idea = '$array_[8]',
+    project_relevance_h4='$array_[9]',
+    project_relevance ='$array_[10]',
+    project_reason_h4 = '$array_[11]',
+    project_reason= '$array_[12]',
+    project_purpose_h4 = '$array_[13]', 
+    project_purpose = '$array_[14]',
+    project_uniqueness_h4 = '$array_[15]', 
+    project_uniqueness = '$array_[16]' 
     WHERE project_id = '$id'";
       if(mysqli_query($conn, $sql)){
           echo "Данные успешно добавлены";
@@ -122,6 +146,36 @@ function add_photos_data_in_dataBase($file_name,$new_id) {
       }
       mysqli_close($conn); 
   }
+  function delete_project($id) {
+    $conn = mysqli_connect("localhost", "root", "122", "movie_db");
+    if (!$conn) {
+      die("Ошибка: " . mysqli_connect_error());
+    }
+    $sql = "DELETE FROM projects WHERE `project_id` = '$id'";
+    if(mysqli_query($conn, $sql)){
+        echo "Данные успешно удалены";
+        
+    } else{
+        echo "Ошибка: " . mysqli_error($conn);
+    }
+    mysqli_close($conn);
+  }
+  function delete_photos($id) {
+    $conn = mysqli_connect("localhost", "root", "122", "movie_db");
+    if (!$conn) {
+      die("Ошибка: " . mysqli_connect_error());
+    }
+    $sql = "DELETE FROM backstage_images WHERE `image_id` = '$id'";
+    if(mysqli_query($conn, $sql)){
+        echo "Данные успешно удалены";
+        
+    } else{
+        echo "Ошибка: " . mysqli_error($conn);
+    }
+    mysqli_close($conn);
+  }
+
+
 ?>
 
 
