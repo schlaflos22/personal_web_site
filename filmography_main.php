ce3ca6852319d5813f89bf39c876c6f1e0bbcd8f
Eva <jenny.rudream@gmail.com>
2022-03-11 22:09:00 +0300

    changed font

--- filmography_main.php
+++ filmography_main.php
@@ -13,7 +13,7 @@
     <title>Document</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/filmography_main.css">
 </head>
 <body>
