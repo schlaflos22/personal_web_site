5e9072c0ec2bd0ebdedc1f042c22d40c6be60d78
Eva <jenny.rudream@gmail.com>
2022-04-08 00:33:58 +0300

    changed title,changed location of mobile-menu, connected js file active_link.js

--- contacts.php
+++ contacts.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0,user-scalable=no">
-    <title>Document</title>
+    <title>Контакты</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
@@ -14,14 +14,27 @@
 <body>
     <div class="wrapper">
         <div class="back-wrapper">
-            <div class="content-wrapper">
-                <div class="languages">
-                    <div class="lang-wrapper">
-                        <span class="lang">RU </span>
-                        <p> / </p>
-                        <span class="lang"> EN</span>
+        <div class="bg"></div>
+            <div class="languages">
+                <div class="mobile-menu">
+                    <input type="checkbox" id="checkbox" class="mobile-menu__checkbox">
+                    <label for="checkbox" class="mobile-menu__btn"><div class="mobile-menu__icon"></div></label>
+                    <div class="mobile-menu__container">
+                        <ul class="mobile-menu__list">
+                            <li class="mobile-menu__item"><a href="index.php" class="mobile-menu__link">Об Авторе</a></li>
+                            <li class="mobile-menu__item"><a href="filmography_main.php" class="mobile-menu__link" >Фильмография</a></li>
+                            <li class="mobile-menu__item"><a href="projects.php" class="mobile-menu__link">Проекты</a></li>
+                            <li class="mobile-menu__item"><a href="contacts.php" class="mobile-menu__link">Контакты</a></li>
+                        </ul>       
                     </div>
                 </div>
+                <div class="lang-wrapper">
+                    <span class="lang">RU </span>
+                    <p> / </p>
+                    <span class="lang"> EN</span>
+                </div>
+            </div>
+            <div class="content-wrapper">
                 <div class="header">
                     <div class="logo" onclick="location.href='index.php'">
                         <img src="img/logo_ .png" alt="">
@@ -33,18 +46,6 @@
                             <li><a href="projects.php" class="link">Проекты</a></li>
                             <li><a href="contacts.php" class="link">Контакты</a></li>
                         </ul>
-                    </div>
-                    <div class="mobile-menu">
-                        <input type="checkbox" id="checkbox" class="mobile-menu__checkbox">
-                        <label for="checkbox" class="mobile-menu__btn"><div class="mobile-menu__icon"></div></label>
-                        <div class="mobile-menu__container">
-                            <ul class="mobile-menu__list">
-                                <li class="mobile-menu__item"><a href="index.php" class="mobile-menu__link">Об Авторе</a></li>
-                                <li class="mobile-menu__item"><a href="filmography_main.php" class="mobile-menu__link" >Фильмография</a></li>
-                                <li class="mobile-menu__item"><a href="projects.php" class="mobile-menu__link">Проекты</a></li>
-                                <li class="mobile-menu__item"><a href="contacts.php" class="mobile-menu__link">Контакты</a></li>
-                            </ul>       
-                        </div>
                     </div>
                 </div>
                 <div class="main">
@@ -76,5 +77,6 @@
            </div>
         </div>
     </div>
+    <script src='js/active_link.js'></script>
 </body>
 </html>
